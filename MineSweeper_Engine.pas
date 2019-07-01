21936768583bdc36b5fd8053f5d18e75b2b2c8bd
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-02 00:54:47 +0500

    Add files via upload

---
+++ MineSweeper_Engine.pas
@@ -0,0 +1,45 @@
+﻿unit MineSweeper_Engine;
+
+interface 
+Uses GraphABC;
+
+const CellSize = 45;
+
+type Cell = class
+  x, y: integer;
+  contains_mine: boolean;
+  revealed: boolean;
+  flag_is_put: boolean;
+  constructor Create(mine: boolean);
+  procedure Click(mouseButton: integer);
+  procedure Draw();
+end;
+
+
+implementation
+
+constructor Cell.Create(mine: boolean);
+begin
+  self.contains_mine := mine;
+  self.x := 0;
+  self.y := 0;
+  self.revealed := false;
+  self.flag_is_put := false;
+end;
+
+procedure Cell.Draw();
+begin
+  if self.revealed then SetBrushColor(rgb(187, 187, 187))
+    else SetBrushColor(rgb(133, 133, 133));
+  Rectangle(self.x, self.y, self.x + CellSize, self.y + CellSize);
+end;
+
+procedure Cell.Click(mouseButton: integer);
+begin
+  if mouseButton = 1 then self.revealed := true;
+  //What if cell contains a bomb ??? ^^^ { if contains_mine then End_party(); }
+  if mouseButton = 2 then self.flag_is_put := not self.flag_is_put;
+end;
+
+
+end.
---
+++ main.pas
@@ -0,0 +1,60 @@
+﻿Uses GraphABC, MineSweeper_Engine;
+
+var grid: array [0..15, 0..15] of Cell;
+    
+procedure Init_Party();
+begin
+  var bomb_count := 40;
+  for var y := 0 to 15 do
+    for var x := 0 to 15 do
+      grid[y, x] := new Cell(false);
+  //Setting Up Bombs
+  while bomb_count <> 0 do
+  begin
+  var x := Random(0, 15);
+  var y := Random(0, 15);
+    if not grid[y, x].contains_mine then grid[y, x].contains_mine := true
+      else continue;
+  bomb_count -= 1;
+  end;
+  
+  //Setting x and y positions for each cell
+  var pos_x := 0;
+  var pos_y := 0;
+  for var y := 0 to 15 do
+    for var x := 0 to 15 do
+    begin
+      grid[y, x].x := pos_x;
+      grid[y, x].y := pos_y;
+      if pos_x + CellSize >= WindowWidth then
+      begin
+        pos_x := 0;
+        pos_y += CellSize;
+      end
+      else pos_x += CellSize;
+    end;
+end;
+
+procedure Draw_Grid();
+begin
+  for var y := 0 to 15 do
+    for var x := 0 to 15 do
+      grid[y, x].Draw();
+  Redraw();
+end;
+
+procedure Main_SetUp();
+begin
+  SetWindowSize(16 * CellSize, 16 * CellSize);
+  Window.CenterOnScreen;
+  Window.IsFixedSize := true;
+  Window.Title := 'MineSweeper';
+  Init_Party();
+end;
+
+begin
+  Main_SetUp();
+  LockDrawing();
+  while true do
+    Draw_Grid();
+end.
