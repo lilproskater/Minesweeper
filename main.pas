9d6b966e2373016c1195a05e5736dedae0aba969
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-06 15:24:53 +0500

    Update main.pas

--- main.pas
+++ main.pas
@@ -9,7 +9,7 @@ begin
   //Setting x and y positions for each cell
   var pos_x := 0;
   var pos_y := 0;
-  for var y := 0 to CellsInRow - 1 do
+  for var y := 0 to CellsInCol - 1 do
     for var x := 0 to CellsInRow - 1 do
     begin
       grid[y, x] := new Cell(pos_x, pos_y, false);
@@ -26,11 +26,31 @@ begin
   while bomb_count <> 0 do
   begin
     var x := Random(0, CellsInRow - 1);
-    var y := Random(0, CellsInRow - 1);
+    var y := Random(0, CellsInCol - 1);
     if not grid[y, x].contains_mine then grid[y, x].contains_mine := true
       else continue;
     bomb_count -= 1;
   end;
+  
+  //Setting Up Numbers
+  for var y := 0 to CellsInCol - 1 do
+    for var x := 0 to CellsInRow - 1 do
+    begin
+      var number := 0;
+      try
+        if grid[y, x - 1].contains_mine then number += 1;
+        if grid[y - 1, x].contains_mine then number += 1;
+        if grid[y + 1, x].contains_mine then number += 1;
+        if grid[y, x + 1].contains_mine then number += 1;
+        if grid[y - 1, x - 1].contains_mine then number += 1;
+        if grid[y - 1, x + 1].contains_mine then number += 1;
+        if grid[y + 1, x - 1].contains_mine then number += 1;
+        if grid[y + 1, x + 1].contains_mine then number += 1;
+      except on System.IndexOutOfRangeException do
+
+      end;
+      grid[y, x].number := number;
+    end;
 end;
 
 procedure EndParty();
@@ -41,7 +61,7 @@ end;
 
 procedure Draw_Grid();
 begin
-  for var y := 0 to CellsInRow - 1 do
+  for var y := 0 to CellsInCol - 1 do
     for var x := 0 to CellsInRow - 1 do
       grid[y, x].Draw();
   Redraw();
@@ -49,16 +69,16 @@ end;
 
 procedure MouseUp(MouseX, MouseY, mouseButton: integer);
 begin
-  if not bomb_is_pressed then
+  if not mine_is_pressed then
   begin
-    var y := Trunc(MouseY / (WindowHeight / CellsInRow));
+    var y := Trunc(MouseY / (WindowHeight / CellsInCol));
     var x := Trunc(MouseX / (WindowWidth / CellsInRow));
     grid[y, x].Click(mouseButton);
     if first_click then 
     begin
-      if bomb_is_pressed then
+      if mine_is_pressed then
       begin
-        bomb_is_pressed := false;
+        mine_is_pressed := false;
         while grid[y, x].contains_mine do
           Init_Party();
         grid[y, x].Click(mouseButton);
@@ -84,7 +104,7 @@ begin
   Main_SetUp();
   while true do
   begin
-    if not bomb_is_pressed then
+    if not mine_is_pressed then
       Draw_Grid()
     else 
       EndParty();
