a5a15f08a80e4f216c67c21b6e94459d76bfad33
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-08 01:00:59 +0500

    Update main.pas

--- main.pas
+++ main.pas
@@ -69,9 +69,9 @@ begin
   if grid[y_grid, x_grid].number <> 0 then exit;
   if not grid[y_grid, x_grid].flag_is_put then grid[y_grid, x_grid].Click(1);
   if y_grid > 0 then OpenCells(y_grid - 1, x_grid);
-  if y_grid < CellsInCol - 1 then OpenCells(y_grid + 1, x_grid);
+  if y_grid < CellsInCol - 2 then OpenCells(y_grid + 1, x_grid);
   if x_grid > 0 then OpenCells(y_grid, x_grid - 1);
-  if x_grid < CellsInRow - 1 then OpenCells(y_grid, x_grid + 1);
+  if x_grid < CellsInRow - 2 then OpenCells(y_grid, x_grid + 1);
   
   //Reveal nearby cells with nubmers
   if y_grid > 0 then if grid[y_grid - 1, x_grid].number <> 0 then grid[y_grid - 1, x_grid].revealed := true;
