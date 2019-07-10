fe9f9fe0ffd28c1afc71bf03259893da4904207e
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-11 00:07:20 +0500

    Add files via upload

---
+++ main.pas
@@ -0,0 +1,35 @@
+﻿Uses GraphABC, MineSweeper_game;
+
+var app_is_running: boolean;
+
+procedure Window_SetUp();
+begin
+  SetWindowSize(CellsInRow * CellSize, CellsInCol * CellSize);
+  Window.CenterOnScreen;
+  Window.Title := 'MineSweeper';
+  Window.IsFixedSize := true;
+end;
+
+procedure Main_SetUp();
+begin
+  Window_SetUp();
+  Init_Party();
+  LockDrawing();
+  OnMouseDown := MouseDown;
+  app_is_running := true;
+end;
+
+procedure ExitApp();
+begin
+  app_is_running := false;
+end;
+
+begin
+  Main_SetUp();
+  while app_is_running do
+  begin
+    OnClose := ExitApp;
+    Draw_Grid();
+    CheckWon();
+  end;
+end.
