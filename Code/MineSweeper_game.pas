0ed8068022dc6faff3f74a0f6160b95b17bdb6cd
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-07-12 01:07:41 +0500

    Update MineSweeper_game.pas

--- Code/MineSweeper_game.pas
+++ Code/MineSweeper_game.pas
@@ -1,12 +1,11 @@
-﻿unit MineSweeper_game;
+unit MineSweeper_game;
 
 interface
 Uses GraphABC, MineSweeper_Engine;
 
-const CellsInCol = 16;
 const CellsInRow = 16;
-const bombsInGrid = 40;
-const CellSize = MineSweeper_Engine.CellSize;
+const bombsInGrid = Round(Sqr(CellsInRow) / 6.4);
+const CellSize = Round(ScreenHeight / CellsInRow / 1.2);
 
 var victory, lose, exit_playing, exit_window_show: boolean;
 
@@ -22,7 +21,7 @@ procedure ExitWindow_MD(MouseX, MouseY, mouseButton: integer);
 
 implementation
 
-var grid: array [0..CellsInCol - 1, 0..CellsInRow - 1] of Cell; 
+var grid: array [0..CellsInRow - 1, 0..CellsInRow - 1] of Cell; 
 
 procedure UpdateWindow();
 begin
@@ -40,19 +39,19 @@ begin
   if grid[y_grid, x_grid].number <> 0 then exit;
   if not grid[y_grid, x_grid].flag_is_put then grid[y_grid, x_grid].Click(1);
   if y_grid > 0 then OpenCells(y_grid - 1, x_grid);
-  if y_grid < CellsInCol - 1 then OpenCells(y_grid + 1, x_grid);
+  if y_grid < CellsInRow - 1 then OpenCells(y_grid + 1, x_grid);
   if x_grid > 0 then OpenCells(y_grid, x_grid - 1);
   if x_grid < CellsInRow - 1 then OpenCells(y_grid, x_grid + 1);
   
   //Reveal nearby cells with nubmers
   if y_grid > 0 then if grid[y_grid - 1, x_grid].number <> 0 then grid[y_grid - 1, x_grid].Click(1);
-  if y_grid < CellsInCol - 1 then if grid[y_grid + 1, x_grid].number <> 0 then grid[y_grid + 1, x_grid].Click(1);
+  if y_grid < CellsInRow - 1 then if grid[y_grid + 1, x_grid].number <> 0 then grid[y_grid + 1, x_grid].Click(1);
   if x_grid > 0 then if grid[y_grid, x_grid - 1].number <> 0 then grid[y_grid, x_grid - 1].Click(1);
   if x_grid < CellsInRow - 1 then if grid[y_grid, x_grid + 1].number <> 0 then grid[y_grid, x_grid + 1].Click(1);
   if (y_grid > 0) and (x_grid > 0) then if grid[y_grid - 1, x_grid - 1].number <> 0 then grid[y_grid - 1, x_grid - 1].Click(1);
   if (y_grid > 0) and (x_grid < CellsInRow - 1) then if grid[y_grid - 1, x_grid + 1].number <> 0 then grid[y_grid - 1, x_grid + 1].Click(1);
-  if (y_grid < CellsInCol - 1) and (x_grid > 0) then if grid[y_grid + 1, x_grid - 1].number <> 0 then grid[y_grid + 1, x_grid - 1].Click(1);
-  if (y_grid < CellsInCol - 1) and (x_grid < CellsInRow - 1) then if grid[y_grid + 1, x_grid + 1].number <> 0 then grid[y_grid + 1, x_grid + 1].Click(1);
+  if (y_grid < CellsInRow - 1) and (x_grid > 0) then if grid[y_grid + 1, x_grid - 1].number <> 0 then grid[y_grid + 1, x_grid - 1].Click(1);
+  if (y_grid < CellsInRow - 1) and (x_grid < CellsInRow - 1) then if grid[y_grid + 1, x_grid + 1].number <> 0 then grid[y_grid + 1, x_grid + 1].Click(1);
 end;
 
 procedure Init_Party();
@@ -66,10 +65,10 @@ begin
   exit_playing := false;
   var pos_x := 0;
   var pos_y := 0;
-  for var y := 0 to CellsInCol - 1 do
+  for var y := 0 to CellsInRow - 1 do
     for var x := 0 to CellsInRow - 1 do
     begin
-      grid[y, x] := new Cell(pos_x, pos_y, false);
+      grid[y, x] := new Cell(pos_x, pos_y, pos_x + CellSize, pos_y + CellSize, false);
       if pos_x + CellSize >= WindowWidth then
       begin
         pos_x := 0;
@@ -83,25 +82,25 @@ begin
   while bombs_counter > 0 do
   begin
     var x := Random(0, CellsInRow - 1);
-    var y := Random(0, CellsInCol - 1);
+    var y := Random(0, CellsInRow - 1);
     if not grid[y, x].contains_mine then grid[y, x].contains_mine := true
       else continue;
     bombs_counter -= 1;    
   end;
   
   //Setting Up Numbers
-  for var y := 0 to CellsInCol - 1 do
+  for var y := 0 to CellsInRow - 1 do
     for var x := 0 to CellsInRow - 1 do
     begin
       var number := 0;
       if x > 0 then if grid[y, x - 1].contains_mine then number += 1;
       if y > 0 then if grid[y - 1, x].contains_mine then number += 1;
-      if y < CellsInCol - 1 then if grid[y + 1, x].contains_mine then number += 1;
+      if y < CellsInRow - 1 then if grid[y + 1, x].contains_mine then number += 1;
       if x < CellsInRow - 1 then if grid[y, x + 1].contains_mine then number += 1;
       if (x > 0) and (y > 0) then if grid[y - 1, x - 1].contains_mine then number += 1;
       if (y > 0) and (x < CellsInRow - 1) then if grid[y - 1, x + 1].contains_mine then number += 1;
-      if (y < CellsInCol - 1) and (x > 0) then if grid[y + 1, x - 1].contains_mine then number += 1;
-      if (y < CellsInCol - 1) and (x < CellsInRow - 1) then if grid[y + 1, x + 1].contains_mine then number += 1;
+      if (y < CellsInRow - 1) and (x > 0) then if grid[y + 1, x - 1].contains_mine then number += 1;
+      if (y < CellsInRow - 1) and (x < CellsInRow - 1) then if grid[y + 1, x + 1].contains_mine then number += 1;
       grid[y, x].number := number;
     end;
 end;
@@ -110,7 +109,7 @@ procedure GameMouseDown(MouseX, MouseY, mouseButton: integer);
 begin
   if not (lose) and not (victory) then
   begin
-    var y := Trunc(MouseY / (WindowHeight / CellsInCol));
+    var y := Trunc(MouseY / (WindowHeight / CellsInRow));
     var x := Trunc(MouseX / (WindowWidth / CellsInRow));
     if mouseButton = 1 then 
     begin
@@ -133,8 +132,8 @@ begin
   end
   else
   begin
-    if (mouseButton = 1) and (MouseX > 20) and (MouseY > WindowHeight - 120) and (MouseX < 120) and (MouseY < WindowHeight - 20) then exit_playing := true;
-    if (mouseButton = 1) and (MouseX > 170) and (MouseY > WindowHeight - 120) and (MouseX < 270) and (MouseY < WindowHeight - 20) then Init_Party();
+    if (mouseButton = 1) and (MouseX > Round(WindowWidth / 36)) and (MouseY > WindowHeight - Round(WindowHeight / 6)) and (MouseX < Round(WindowWidth / 6)) and (MouseY < WindowHeight - Round(WindowHeight / 36)) then exit_playing := true;
+    if (mouseButton = 1) and (MouseX > Round(WindowWidth /4.235)) and (MouseY > WindowHeight - Round(WindowHeight / 6)) and (MouseX < Round(WindowWidth / 2.666)) and (MouseY < WindowHeight - Round(WindowHeight / 36)) then Init_Party();
   end; 
 end;
 
@@ -142,39 +141,39 @@ procedure Draw_Grid();
 begin
   SetPenWidth(1);
   SetPenColor(rgb(0, 0, 0));
-  for var y := 0 to CellsInCol - 1 do
+  for var y := 0 to CellsInRow - 1 do
     for var x := 0 to CellsInRow - 1 do
       grid[y, x].Draw();
   if lose then
   begin
     SetFontColor(clRed);
-    SetFontSize(100);
+    SetFontSize(Round(WindowHeight / 7.2));
     DrawTextCentered(0, 0, WindowWidth, WindowHeight, 'You Lose!');
-    SetFontSize(50);
-    SetPenWidth(7);
+    SetFontSize(Round(WindowHeight / 14.4));
+    SetPenWidth(Round(WindowHeight / 102.857));
     SetPenColor(rgb(255, 255, 255));
     SetBrushColor(rgb(185, 185, 185));
-    Rectangle(20, WindowHeight - 120, 120, WindowHeight - 20);
-    Rectangle(170, WindowHeight - 120, 270, WindowHeight - 20);
+    Rectangle(Round(WindowWidth / 36), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 6), WindowHeight - Round(WindowHeight / 36));
+    Rectangle(Round(WindowWidth /4.235), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 2.666), WindowHeight - Round(WindowHeight / 36));
     SetFontColor(rgb(255, 255, 255));
-    DrawTextCentered(20, WindowHeight - 120, 120, WindowHeight - 20, '←');
-    DrawTextCentered(170, WindowHeight - 120, 270, WindowHeight - 20, '►');
+    DrawTextCentered(Round(WindowWidth / 36), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 6), WindowHeight - Round(WindowHeight / 36), '←');
+    DrawTextCentered(Round(WindowWidth /4.235), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 2.666), WindowHeight - Round(WindowHeight / 36), '►');
   end;
   if victory then
   begin
     SetFontColor(clLime);
-    SetFontSize(100);
+    SetFontSize(Round(WindowHeight / 7.2));
     DrawTextCentered(0, 0, WindowWidth, WindowHeight, 'You Won!');
-    SetFontSize(50);
-    SetPenWidth(7);
+    SetFontSize(Round(WindowHeight / 14.4));
+    SetPenWidth(Round(WindowHeight / 102.857));
     SetPenColor(rgb(255, 255, 255));
     SetBrushColor(rgb(185, 185, 185));
-    Rectangle(20, WindowHeight - 120, 120, WindowHeight - 20);
-    Rectangle(170, WindowHeight - 120, 270, WindowHeight - 20);
+    Rectangle(Round(WindowWidth / 36), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 6), WindowHeight - Round(WindowHeight / 36));
+    Rectangle(Round(WindowWidth /4.235), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 2.666), WindowHeight - Round(WindowHeight / 36));
     SetFontColor(rgb(255, 255, 255));
-    DrawTextCentered(20, WindowHeight - 120, 120, WindowHeight - 20, '←');
-    DrawTextCentered(170, WindowHeight - 120, 270, WindowHeight - 20, '►');
-  end;
+    DrawTextCentered(Round(WindowWidth / 36), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 6), WindowHeight - Round(WindowHeight / 36), '←');
+    DrawTextCentered(Round(WindowWidth /4.235), WindowHeight - Round(WindowHeight / 6), Round(WindowWidth / 2.666), WindowHeight - Round(WindowHeight / 36), '►');
+ end;
   UpdateWindow();
 end;
 
@@ -185,7 +184,7 @@ begin
   else
   begin
     var count_unrevealed := 0;
-    for var y := 0 to CellsInCol - 1 do
+    for var y := 0 to CellsInRow - 1 do
       for var x := 0 to CellsInRow - 1 do
        if not grid[y, x].revealed then count_unrevealed += 1;
     if count_unrevealed = bombsInGrid then victory := true;
@@ -207,27 +206,27 @@ begin
   SetPenColor(rgb(255, 255, 255));
   SetBrushColor(rgb(185, 185, 185));
   SetFontColor(rgb(255, 255, 255));
-  SetPenWidth(7);
-  Rectangle(50, 50, WindowWidth - 50, WindowHeight - 50);
-  SetFontSize(35);
-  DrawTextCentered(80, 70, WindowWidth - 50, 200, 'Вы действительно хотите покинуть игру?');
-  Rectangle(220, WindowHeight - 200, 320, WindowHeight - 100);
-  Rectangle(370, WindowHeight - 200, 470, WindowHeight - 100);
-  SetFontSize(30);
-  DrawTextCentered(220, WindowHeight - 200, 320, WindowHeight - 100, 'Да');
-  DrawTextCentered(370, WindowHeight - 200, 470, WindowHeight - 100, 'Нет');
+  SetPenWidth(Round(WindowHeight / 102.835));
+  Rectangle(Round(WindowWidth / 14.4), Round(WindowHeight / 14.4), WindowWidth - Round(WindowWidth / 14.4), WindowHeight - Round(WindowHeight / 14.4));
+  SetFontSize(Round(WindowHeight / 20.571));
+  DrawTextCentered(Round(WindowWidth / 9), Round(WindowHeight / 10.285), WindowWidth - Round(WindowWidth / 14.4), Round(WindowHeight / 3.6), 'Вы действительно хотите покинуть игру?');
+  Rectangle(Round(WindowWidth / 3.272), WindowHeight - Round(WindowHeight / 3.6), Round(WindowWidth / 2.25), WindowHeight - Round(WindowHeight / 7.2));
+  Rectangle(Round(WindowWidth / 1.945), WindowHeight - Round(WindowHeight / 3.6), Round(WindowWidth / 1.531), WindowHeight - Round(WindowHeight / 7.2));
+  SetFontSize(Round(WindowHeight / 24));
+  DrawTextCentered(Round(WindowWidth / 3.272), WindowHeight - Round(WindowHeight / 3.6), Round(WindowWidth / 2.25), WindowHeight - Round(WindowHeight / 7.2), 'Да');
+  DrawTextCentered(Round(WindowWidth / 1.945), WindowHeight - Round(WindowHeight / 3.6), Round(WindowWidth / 1.531), WindowHeight - Round(WindowHeight / 7.2), 'Нет');
   UpdateWindow();
 end;
 
 procedure ExitWindow_MD(MouseX, MouseY, mouseButton: integer);
 begin
-  if (mouseButton = 1) and (MouseX > 220) and (MouseY > WindowHeight - 200) and (MouseX < 320) and (MouseY < WindowHeight - 100) then
+  if (mouseButton = 1) and (MouseX > Round(WindowWidth / 3.272)) and (MouseY > WindowHeight - Round(WindowHeight / 3.6)) and (MouseX < Round(WindowWidth / 2.25)) and (MouseY < WindowHeight - Round(WindowHeight / 7.2)) then
   begin
     exit_window_show := false;
     exit_playing := true;
   end;
   
-  if (mouseButton = 1) and (MouseX > 370) and (MouseY > WindowHeight - 200) and (MouseX < 470) and (MouseY < WindowHeight - 100) then
+  if (mouseButton = 1) and (MouseX > Round(WindowWidth / 1.945)) and (MouseY > WindowHeight - Round(WindowHeight / 3.6)) and (MouseX < Round(WindowWidth / 1.531)) and (MouseY < WindowHeight - Round(WindowHeight / 7.2)) then
     exit_window_show := false;
 end;
 
